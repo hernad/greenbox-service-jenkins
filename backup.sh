cbb1c91603ff2f6d4b5a47ede555a0e6bbb8ad8a
Ernad Husremovic <hernad@bring.out.ba>
2017-04-12 12:17:42 +0200

    backup xz

---
+++ backup.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+if ! rclone listremotes  | grep -q gdrive_bout
+then
+  echo "rclone mora biti konfigurisan - podesen remote gdrive_bout"
+  exit 1
+fi
+
+
+docker stop jenkins-1
+
+tar cvfJ jenkins_home.tar.xz jenkins_home
+
+docker start jenkins-1
