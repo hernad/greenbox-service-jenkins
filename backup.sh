d0c04926583cba7fbd5eaf0f9b3839b1199f7b5c
Ernad Husremovic <hernad@bring.out.ba>
2017-04-22 09:00:05 +0200

    update backup

--- backup.sh
+++ backup.sh
@@ -7,10 +7,17 @@ then
 fi
 
 
+
+if [ ! -d jenkins_home ]
+then
+	echo "We are not on right location: $(pwd) ?!"
+	exit 1
+fi
+
+echo "stop jenkins-1"
 docker stop jenkins-1
-
 tar cvfJ jenkins_home.tar.xz jenkins_home
-
+echo "start jenkins-1"
 docker start jenkins-1
 
 rclone copy -v jenkins_home.tar.xz gdrive_bout:/jenkins_home.tar.xz
