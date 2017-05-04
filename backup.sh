8fe4d3bd2b1c8696afeb9c8d3834e7a5661260e3
Ernad Husremovic <hernad@bring.out.ba>
2017-05-04 07:06:33 +0200

    update backup

--- backup.sh
+++ backup.sh
@@ -20,5 +20,6 @@ tar cvfJ jenkins_home.tar.xz jenkins_home
 echo "start jenkins-1"
 docker start jenkins-1
 
-rclone copy -v gdrive_bout:/jenkins_home/jenkins_home.tar.xz gdrive_bout:/jenkins_home/jenkins_home.tar.xz.1
+rclone delete -v gdrive_bout:/jenkins_home/1/jenkins_home.tar.xz
+rclone copy -v gdrive_bout:/jenkins_home/jenkins_home.tar.xz gdrive_bout:/jenkins_home/1
 rclone copy -v jenkins_home.tar.xz gdrive_bout:/jenkins_home/
